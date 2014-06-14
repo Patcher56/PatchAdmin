12797dad13484bcfc15694c1aab63326a6a33ba5
Patcher56 <patrick.kuen11@gmail.com>
2014-06-15 00:05:00 +0200

    Updated workshopupdater.bat

--- workshopupdater.bat
+++ workshopupdater.bat
@@ -1,5 +1,5 @@
-"../../../bin/gmad.exe" create -out "D:\Benutzer\Patrick\Dateien\Workshop\PAdmin.gma" -folder "D:\Benutzer\Patrick\Dateien\Server\Valve-Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
+"../../../bin/gmad.exe" create -out "D:\Daten\Workshop\PAdmin.gma" -folder "D:\Daten\Server\Valve Server\steamapps\common\GarrysModDS\garrysmod\addons\PatchProtect"
 
-"../../../bin/gmpublish" update -addon "D:\Benutzer\Patrick\Dateien\Workshop\PAdmin.gma" -id "183047564" -changes "Fixed a few bugs"
+"../../../bin/gmpublish" update -addon "D:\Daten\Workshop\PAdmin.gma" -id "183047564" -changes "UPDATE INFOS WILL SHOW UP HERE SOON"
 
 PAUSE
